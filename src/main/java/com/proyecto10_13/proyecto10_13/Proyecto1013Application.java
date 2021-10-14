356c371e550a135d928e8742148234d88ceb9ba5
JUAN CARLOS RAMIREZ <juan.ramirez2.mt@correo.usa.edu.co>
2021-10-13 21:36:33 -0500

    proyecto Spring boot

---
+++ src/main/java/com/proyecto10_13/proyecto10_13/Proyecto1013Application.java
@@ -0,0 +1,13 @@
+package com.proyecto10_13.proyecto10_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1013Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto10_13/proyecto10_13/Proyecto1013ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto10_13.proyecto10_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1013ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
